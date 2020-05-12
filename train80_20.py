ddf9f272b9d72f97e66c28196db0f4e28a203429
YuryM <ypmunoz36@ucatolica.edu.co>
2020-05-12 18:32:33 -0500

    Segmentador de frames , entrenamiento 75 25 y 80 20

---
+++ train80_20.py
@@ -0,0 +1,147 @@
+# USO 
+# python train.py --dataset /data --model model/activity.model --label-bin model/lb.pickle --epochs 100
+
+import matplotlib
+matplotlib.use("Agg")
+
+# Importacion de paquetes necesarios
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.pooling import AveragePooling2D
+from keras.applications import ResNet50
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+import os
+
+# Se reciben los argumentos
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output serialized model")
+ap.add_argument("-l", "--label-bin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-e", "--epochs", type=int, default=25,
+	help="# of epochs to train our network for")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# Se inicializa un arreglo de clases 
+LABELS = set(["bebe", "anciano", "hombre","homosexual","joven"])
+
+print("[INFO] Cargando fotogramas...")
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
+labels = []
+
+# recorre las rutas de las carpetas
+for imagePath in imagePaths:
+  label = imagePath.split(os.path.sep)[-2]
+
+  if label not in LABELS:
+    continue
+
+#Lectura de cada fotograma
+  image = cv2.imread(imagePath)
+  data.append(image)
+  labels.append(label)
+
+#arreglos NumPy
+data = np.array(data)
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+# divide el conjunto de datos en training y testing
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+#Inicializa el entrenamiento 
+trainAug = ImageDataGenerator(
+	rotation_range=30,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+
+valAug = ImageDataGenerator()
+
+mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+trainAug.mean = mean
+valAug.mean = mean
+
+#Carga la red ResNet-50 
+baseModel = ResNet50(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(512, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(len(lb.classes_), activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+	layer.trainable = False
+
+#Compila el modelo 
+print("[INFO] Compilando el modelo...")
+opt = SGD(lr=1e-4, momentum=0.9, decay=1e-4 / args["epochs"])
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+print("[INFO] training head...")
+H = model.fit_generator(
+	trainAug.flow(trainX, trainY, batch_size=32),
+	steps_per_epoch=len(trainX) // 32,
+	validation_data=valAug.flow(testX, testY),
+	validation_steps=len(testX) // 32,
+	epochs=args["epochs"])
+
+# Evaluar la red 
+print("[INFO] Evaludando la red...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+	predictions.argmax(axis=1), target_names=lb.classes_))
+
+# Grafico de training loss y accuracy
+N = args["epochs"]
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_accuracy")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_accuracy")
+plt.title("-->Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
+
+# Serializa el modelo y lo guarda en disco
+print("[INFO] serializing network...")
+model.save(args["model"])
+
+# serialize the label binarizer to disk
+f = open(args["label_bin"], "wb")
+f.write(pickle.dumps(lb))
+f.close()
